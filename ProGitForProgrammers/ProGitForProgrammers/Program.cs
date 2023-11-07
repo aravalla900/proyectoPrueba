1c0e73eed34669c14ff55f9cc6833ff1cb362cb8
aravalla900 <aravalla900@outlook.com>
2023-11-07 13:49:57 -0600

    Add writeline indicating we are in command line

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -8,6 +8,7 @@ namespace ProGitForProgrammers
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this line in visual studio");
+            Console.WriteLine("I just added this line in the command line");
         }
     }
 }
