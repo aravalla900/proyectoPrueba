e42017dd36f90e0211f60290e4a1786a444debdb
aravalla900 <aravalla900@outlook.com>
2023-11-08 10:32:42 -0600

    New line from vscode

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -9,6 +9,8 @@ namespace ProGitForProgrammers
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this line in visual studio");
             Console.WriteLine("I just added this line in the command line");
+
+            Console.WriteLine("New line from vscode");
         }
     }
 }
